1851d05f3668fc522e8f062c1bdec2f1e5a05794
Artur Doruch <arturdoruch@interia.pl>
2021-04-06 17:56:19 +0200

    Define bundle services configuration in xml file

--- DependencyInjection/ArturDoruchDoctrineEntityManagerExtension.php
+++ DependencyInjection/ArturDoruchDoctrineEntityManagerExtension.php
@@ -4,7 +4,7 @@ namespace ArturDoruch\DoctrineEntityManagerBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
@@ -14,7 +14,7 @@ class ArturDoruchDoctrineEntityManagerExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
     }
 }
